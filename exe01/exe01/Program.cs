f9daa1f1a89037a6b05277572a8dbc72b3984d5e
Jackeline Evellyn <jackelineeteixeira@gmail.com>
2019-10-11 13:47:27 -0300

    first commit

---
+++ exe01/exe01/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace exe01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = 10;
+            var y = 9.9;
+            var z = "Maria";
+
+            //Exemplo de erro
+            //int z = y;
+
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(z);
+        }
+    }
+}
