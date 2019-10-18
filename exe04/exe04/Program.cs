7b9251c9a89e562788b82a4519717e29519c6f7f
Jackeline Evellyn <jackelineeteixeira@gmail.com>
2019-10-18 09:51:10 -0300

    Update

--- exe04/exe04/Program.cs
+++ exe04/exe04/Program.cs
@@ -6,7 +6,7 @@ namespace exe04
     {
         static void Main(string[] args)
         {
-            string original = "abcde FGHIJ ABC abc DEFG";
+            string original = "  abcde FGHIJ ABC abc DEFG ";
 
             string s1 = original.ToUpper();
             string s2 = original.ToLower();
@@ -15,7 +15,14 @@ namespace exe04
             int n1 = original.IndexOf("bc"); // primeira aparição
             int n2 = original.LastIndexOf("bc"); //última aparição
 
+            string s4 = original.Substring(3); // começando na posição 3
+            string s5 = original.Substring(3, 5); // Do 3 ao 5
 
+            string s6 = original.Replace('a', 'x'); //substitui todo caratere a por x
+            string s7 = original.Replace("abc","xyz"); //Substritui um conjunto de caractere
+
+            bool b1 = String.IsNullOrEmpty(original);
+            bool b2 = String.IsNullOrWhiteSpace(original);
 
             Console.WriteLine("Original: " + original + ".");
             Console.WriteLine("ToUpper: " + s1 + ".");
@@ -24,6 +31,16 @@ namespace exe04
 
             Console.WriteLine("Primeira aparição: " + n1 + ".");
             Console.WriteLine("Última aparição: " + n2 + ".");
+             
+            Console.WriteLine("Substring(3): " + s4);
+            Console.WriteLine("Substring(3, 5): " + s5);
+
+            Console.WriteLine("Replace('a','x'): "+s6);
+            Console.WriteLine("Replace('abc','xyz'): " + s7);
+
+            Console.WriteLine("Is null or Empty: "+b1);
+            Console.WriteLine("Is null or White Spare: "+b2);
+
         }
     }
 }
---
+++ exe05/exe05/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace exe05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime d1 = DateTime.Now;
+            // A quantidade de ticks que já se passaram desde o inicio era 0001
+
+            DateTime d2 = new DateTime(2019, 10, 17);
+            DateTime d3 = new DateTime(2019, 10, 17, 20, 45, 2);
+            DateTime d4 = new DateTime(2019, 10, 17, 20, 45, 2, 500);
+
+            DateTime d5 = DateTime.Now;
+            DateTime d6 = DateTime.Today;
+            DateTime d7 = DateTime.UtcNow; //Horario atual, porém no Grenwich
+
+            Console.WriteLine(d2);
+            Console.WriteLine(d3);
+            Console.WriteLine(d4);
+
+            Console.WriteLine(d1);
+            Console.WriteLine(d1.Ticks);
+            Console.WriteLine(d5);
+            Console.WriteLine(d6);
+            Console.WriteLine(d7);
+
+            Console.WriteLine();
+
+            DateTime d8 = DateTime.Parse("2000-08-15"); // Converter de string para datetime
+            DateTime d9 = DateTime.Parse("2000-08-15 ")
+
+            Console.WriteLine(d8);
+     }
+    }
+}
